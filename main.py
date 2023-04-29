71349c0b0519f14d42044ebf96dde2be4315f0e1
Guido Rocatti <117786253+guipo-r@users.noreply.github.com>
2023-04-29 20:52:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from replit import clear
 
 def play():
   print(logo)
-  print("Welcome to the Number Guesser game!")
+  print("Welcome to the Number Guessing Game!")
   print("I'm thinking of a number between 1 and 100")
 
 
